ca93b57d5f64322b74d40eb0972fc36db3125fb9
NotBioWaste905 <andruhabobr@gmail.com>
2025-11-28 15:59:57 +0300

    gotcha

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	export const prerender = true;
+    export const trailingSlash = 'always';
 	
 	let { children } = $props();
 </script>
